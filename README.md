373ce3b836fecc37c850944e1dcd8e177e90a7ab
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-05-31 17:27:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ICS2O-PWA-Template-HTML
+
+Template to make an MDL, PWA website, on GitHub pages.
+
+[![Mr Coxall's Super Linter](https://github.com/mr-coxall/ICS2O-Assignment-06-Example/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/mr-coxall/ICS2O-Assignment-06-Example/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/mr-coxall/ICS2O-Assignment-06-Example)](https://repl.it/github/mr-coxall/ICS2O-Assignment-06-Example)
+
+This site can be found at: [https://mr-coxall.github.io/ICS2O-Assignment-06-Example/](https://mr-coxall.github.io/ICS2O-Assignment-06-Example/)
---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Assignment-06-Example/sw.js", {
+    scope: "/ICS2O-Assignment-06-Example/",
+  })
+}
+
+
+/**
+ * Get API info.
+*/
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
+
+const getJoke = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("joke").innerHTML =
+    '<joke_id' + 
+      jsonData.url + 
+      '" alt="API joke" class="center" ' +
+      '>'
+    if (jsonData.artist_url != "none") {
+      document.getElementById("image-artist").innerHTML =
+      "<p>Artist: " +
+      '<a href="' +
+      jsonData.artist_url +
+      '">' +
+      jsonData.artist +
+      "</a>"
+  } else {
+    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
+  }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getJoke("https://icanhazdadjoke.com/j/<joke_id>")
